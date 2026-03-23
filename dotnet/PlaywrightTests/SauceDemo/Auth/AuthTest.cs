9b6f1a772132abdcfd4e979048cc4d1d6eab3f96
ravihlb <ravihlb@protonmail.com>
2026-03-23 14:49:18 -0300

    chore: remove some comments

--- dotnet/PlaywrightTests/SauceDemo/Auth/AuthTest.cs
+++ dotnet/PlaywrightTests/SauceDemo/Auth/AuthTest.cs
@@ -6,20 +6,17 @@ namespace PlaywrightTests;
 public class AuthenticatePageTests : PageTest
 {
 
-  // same password for all users
   const string password = "secret_sauce";
   AuthPage authPage = null!;
 
   override public async Task InitializeAsync()
   {
-    Console.WriteLine("Test Setup step");
     await base.InitializeAsync();
     authPage = new AuthPage(Page);
   }
 
   public override async Task DisposeAsync()
   {
-    Console.WriteLine("After each test cleanup");
     await base.DisposeAsync();
   }
 
@@ -40,30 +37,25 @@ public class AuthenticatePageTests : PageTest
   [Fact]
   public async Task Negative_WrongPassword()
   {
-    // Arrange
     const string username = "locked_out_user";
     const string wrongPassword = "randomwrongpassword";
 
-    // Act
     await authPage.GotoAsync();
     await authPage.LoginAsync(username, wrongPassword);
 
-    // Asssert
-    await Expect(Page).Not.ToHaveURLAsync(new Regex(".*/inventory"));
     await Expect(authPage.Elements.errorMessage).ToBeVisibleAsync();
+    await Expect(Page).Not.ToHaveURLAsync(new Regex(".*/inventory"));
   }
 
   [Fact]
   public async Task Negative_LockedOutUser()
   {
-    // Arrange
     const string username = "locked_out_user";
 
-    // Act
     await authPage.GotoAsync();
     await authPage.LoginAsync(username, password);
 
-    // Asssert
+    await Expect(authPage.Elements.errorMessage).ToBeVisibleAsync();
     await Expect(Page).Not.ToHaveURLAsync(new Regex(".*/inventory"));
   }
 }
